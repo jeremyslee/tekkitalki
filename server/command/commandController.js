baca602e01fe4c7c0b16b965e884d74900129fd0
Jeremy S. Lee <jeremysanglee@gmail.com>
2017-10-13 15:09:55 -0700

    Add addCommand to commandController

--- server/command/commandController.js
+++ server/command/commandController.js
@@ -5,4 +5,13 @@ commandController.findCommand = (req, res) => {
 
 };
 
+commandController.addCommand = (req, res) => {
+    let cmdToSave = req.body;
+    let command = new Command(cmdToSave);
+    command.save((err) => {
+        if (err) console.log('Error: could not save');
+        console.log("Saved successfully");
+    });
+}
+
 module.exports = commandController;
