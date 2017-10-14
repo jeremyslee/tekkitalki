bfeb830ad9352c916dd02ed9fab65445fd75d4ae
freemanchen <freemanxchen@gmail.com>
2017-10-14 14:03:17 -0700

    refactor grid.js and auto generate commandString

--- client/javascripts/components/grid.js
+++ client/javascripts/components/grid.js
@@ -24,28 +24,17 @@ class Grid extends React.Component{
     }
     this.oneCommand = this.oneCommand.bind(this);
     this.camelize = this.camelize.bind(this);
+    this.getLineNumber = this.getLineNumber.bind(this);
+    this.numWords = { 'one': 1, 'two': 2, 'to': 2, 'too': 2, 'three': 3, 'four': 4, 'for' : 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'ten': 10, 'eleven': 11, 'twelve': 12, 'thirteen': 13, 'fourteen': 14, 'fifteen': 15 };
   }
 
   parseSpeech() {
     let string = this.state.speech.trim();
     let insertWhere;
-    let matchLine;
-    let numWords = { 'one': 1, 'two': 2, 'to': 2, 'too': 2, 'three': 3, 'four': 4, 'for' : 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'ten': 10, 'eleven': 11, 'twelve': 12, 'thirteen': 13, 'fourteen': 14, 'fifteen': 15 }
+    let numWords = this.numWords;
+    string = string.replace('the clara', 'declare a').replace('sinclair', 'declare');
+    [string, insertWhere] = this.getLineNumber(string);
 
-    // regex returns array ['line number 5', '5']
-    matchLine = string.match(/(?:on line number\ *)(\w+)/);
-    if (!matchLine) matchLine = string.match(/(?:on line\ *)(\w+)/);
-    if (!matchLine) matchLine = string.match(/(?:online number\ *)(\w+)/);
-    if (!matchLine) matchLine = string.match(/(?:online\ *)(\w+)/);
-    if (!matchLine) matchLine = string.match(/(?:on mine\ *)(\w+)/);
-    if (matchLine && matchLine.length > 1) {
-      insertWhere = parseInt(matchLine[1]);
-      if (isNaN(insertWhere)) insertWhere = parseInt(numWords[matchLine[1]]);
-      if (isNaN(insertWhere)) insertWhere = undefined;
-    }
-    if (insertWhere === undefined) insertWhere = this.state.onPageCode.length + 2;
-
-    string = string.replace(/(?:on line number\ *)(\w+)/, '').replace(/(?:on line\ *)(\w+)/, '').replace(/(?:online number\ *)(\w+)/, '').replace(/(?:online\ *)(\w+)/, '').replace(/(?:on mine\ *)(\w+)/, '');
     let self = this;
     const commands = this.state.allCommands;
     for (let i = 0; i < commands.length; i += 1) {
@@ -64,41 +53,63 @@ class Grid extends React.Component{
     console.log('command to find ', string);
     console.log('vars to keep ', this.state.commandVars);
     console.log('insert at', this.state.insertWhere);
-    // LOOK UP COMMAND IN DB
 
+    // LOOK UP COMMAND IN DB
     let xhr = new XMLHttpRequest();
     xhr.open("GET", this.props.baseUrl + '/find/?command=' + encodeURIComponent(string));
 
     xhr.onreadystatechange = () => {
       if (xhr.readyState == 4 && xhr.status == 200) {
         if (xhr.responseText === 'NA') {
-          console.log('No such command.');
 
+          console.log('No such command.');
           artyom.initialize({
-                        lang:"en-GB",
-                        continuous:false,
-                        listen:false,
-                        debug:false,
-                        speed:1
-                    }).then(function(){
-                        artyom.say('Sorry, your technical communication is poor.');
-                        self.setState({
-                          listenFail: true
-                        });
-                    })
+            lang:"en-GB",
+            continuous:false,
+            listen:false,
+            debug:false,
+            speed:1
+          }).then(function(){
+            artyom.say('Sorry, your technical communication is poor.');
+            self.setState({
+              listenFail: true
+            });
+          })
         }
         else {
           let obj = JSON.parse(xhr.responseText);
+
+          // Runs all the functionality saved on the database object
+          // i.e WHERE THE MAGIC HAPPENS
           eval(obj.fn);
 
           self.setState({
             lineCount: Math.max(self.state.onPageCode.length, 9)
-          })
+          });
         }
       }
     }
     xhr.send();
+  }
 
+  getLineNumber(str) {
+    let string = str;
+    let matchLine, insertWhere;
+    // regex returns array ['line number 5', '5']
+    matchLine = string.match(/(?:on line number\ *)(\w+)/);
+    if (!matchLine) matchLine = string.match(/(?:on line\ *)(\w+)/);
+    if (!matchLine) matchLine = string.match(/(?:online number\ *)(\w+)/);
+    if (!matchLine) matchLine = string.match(/(?:online\ *)(\w+)/);
+    if (!matchLine) matchLine = string.match(/(?:on mine\ *)(\w+)/);
+    if (matchLine && matchLine.length > 1) {
+      insertWhere = parseInt(matchLine[1]);
+      if (isNaN(insertWhere)) insertWhere = parseInt(this.numWords[matchLine[1]]);
+      if (isNaN(insertWhere)) insertWhere = undefined;
+    }
+    if (insertWhere === undefined) insertWhere = this.state.onPageCode.length + 2;
+
+    string = string.replace(/(?:on line number\ *)(\w+)/, '').replace(/(?:on line\ *)(\w+)/, '').replace(/(?:online number\ *)(\w+)/, '').replace(/(?:online\ *)(\w+)/, '').replace(/(?:on mine\ *)(\w+)/, '');
+    return [string, insertWhere]
   }
 
   camelize(str) {
@@ -115,26 +126,26 @@ class Grid extends React.Component{
       listenFail: false
     })
     artyom.initialize({
-                  lang:"en-GB",
-                  continuous:false,
-                  listen:true,
-                  debug:false,
-                  speed:1
-              }).then(function(){
-                  console.log("Ready to work !");
-              })
+      lang:"en-GB",
+      continuous:false,
+      listen:true,
+      debug:false,
+      speed:1
+    }).then(function(){
+      console.log("Ready to work !");
+    });
 
     artyom.on(['*'] , true).then((i, result) => {
-                  self.setState({
-                    speech: result,
-                    listening: false
-                  });
-                  // artyom.say("There you go!");
-                  artyom.fatality();
-                  setTimeout(function() {
-                    self.parseSpeech();
-                  }, 0);
-              });;
+      self.setState({
+        speech: result,
+        listening: false
+      });
+      // artyom.say("There you go!");
+      artyom.fatality();
+      setTimeout(function() {
+        self.parseSpeech();
+      }, 0);
+    });
   }
 
   componentDidMount() {
--- server/command/command_lisst.js
+++ server/command/command_lisst.js
@@ -1,41 +1,35 @@
 {
 	"commands": ["declare a variable", "initialize a variable"],
-	"commandString": "declare a variable, initialize a variable",
 	"code": "let ??;",
 	"fn": "let variable = self.state.commandVars.slice(-1)[0]; if (variable === undefined) variable = 'x'; let code = obj.code; code = code.replace('??', self.camelize(variable)); let arr = self.state.onPageCode; arr.splice(self.state.insertWhere - 1, 0, '<p>' + code + '</p>'); self.setState({ onPageCode: arr, commandVars: self.state.commandVars.slice(0, self.state.commandVars.length - 1), existingVars: self.state.existingVars.concat(self.camelize(variable)) });"
 }
 
 {
 	"commands": ["console log", "console.log", "log out"],
-	"commandString": "console log, console.log, log out",
 	"code": "console.log(??);",
 	"fn": "let code = obj.code; let variable = self.state.commandVars.slice(-1)[0]; if (variable === undefined) { variable = '' } else if (self.state.existingVars.indexOf(self.camelize(variable)) > -1) { variable = self.camelize(variable); } else { variable = '&#39;' + variable + '&#39;'; } code = code.replace('??', variable); let arr = self.state.onPageCode; arr.splice(self.state.insertWhere - 1, 0, '<p>' + code + '</p>'); self.setState({ onPageCode: arr, commandVars: self.state.commandVars.slice(0, self.state.commandVars.length - 1) });"
 }
 
 {
 	"commands": ["insert a new line before line number", "insert a new line before line", "create a new line before line number", "create a new line before line"],
-	"commandString": "insert a new line before line number,insert a new line before line,create a new line before line number,create a new line before line",
 	"code": "EMPTY",
 	"fn": "let lineNumber = parseInt(self.state.commandVars.slice(-1)[0]); if (isNaN(lineNumber)) lineNumber = parseInt(numWords[self.state.commandVars.slice(-1)[0]]); let arr = self.state.onPageCode; if (!isNaN(lineNumber)) arr.splice(lineNumber - 1, 0, '<p>&nbsp;</p>'); self.setState({ onPageCode: arr, commandVars: self.state.commandVars.slice(0, self.state.commandVars.length - 1) });"
 }
 
 {
 	"commands": ["insert a new line after line number", "insert a new line after line", "create a new line after line number", "create a new line after line"],
-	"commandString": "insert a new line after line number,insert a new line after line,create a new line after line number,create a new line after line",
 	"code": "EMPTY",
 	"fn": "let lineNumber = parseInt(self.state.commandVars.slice(-1)[0]); if (isNaN(lineNumber)) lineNumber = parseInt(numWords[self.state.commandVars.slice(-1)[0]]); let arr = self.state.onPageCode; if (!isNaN(lineNumber)) arr.splice(lineNumber, 0, '<p>&nbsp;</p>'); self.setState({ onPageCode: arr, commandVars: self.state.commandVars.slice(0, self.state.commandVars.length - 1) });"
 }
 
 {
 	"commands": ["delete line number", "remove line number"],
-	"commandString": "delete line number, remove line number",
 	"code": "LINE DELETED",
 	"fn": "let lineNumber = parseInt(self.state.commandVars.slice(-1)[0]); if (isNaN(lineNumber)) lineNumber = parseInt(numWords[self.state.commandVars.slice(-1)[0]]); let arr = self.state.onPageCode; arr.splice(lineNumber - 1, 1); self.setState({ onPageCode: arr, commandVars: self.state.commandVars.slice(0, self.state.commandVars.length - 1) });"
 }
 
 {
 	"commands": ["declare a function called", "create a function called", "define a function called"],
-	"commandString": "declare a function called, create a function called, define a function called",
 	"code": "function ??() {",
 	"fn": "let funcName = self.state.commandVars.slice(-1)[0]; if (variable === undefined) variable = 'foo'; let code = obj.code; code = code.replace('??', self.camelize(funcName)); let arr = self.state.onPageCode; arr.splice(self.state.insertWhere - 1, 0, '<p>' + code + '</p>'); arr.splice(self.state.insertWhere, 0, '<p>&nbsp;</p>'); arr.splice(self.state.insertWhere + 1, 0, '<p>};</p>'); self.setState({ onPageCode: arr, commandVars: self.state.commandVars.slice(0, self.state.commandVars.length - 1), existingVars: self.state.existingVars.concat(self.camelize(funcName)) });"
 }
